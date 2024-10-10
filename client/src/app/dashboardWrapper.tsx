5dc3ee7657e64d796bf1bae91606873100ffba0a
Mateusz Kurek <m.kurek1994@gmail.com>
2024-10-11 00:18:40 +0200

    Add redux, sidebar and handle dark mode

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -1,21 +1,53 @@
-import React from "react";
-import Navbar from '@/app/(components)/Navbar'
+"use client";
+
+import React, { useEffect } from "react";
+import Navbar from "@/app/(components)/Navbar";
+import Sidebar from "@/app/(components)/Sidebar";
+import StoreProvider, { useAppSelector } from "./redux";
 
 type DashboardWrapperProps = {
   children: React.ReactNode;
 };
 
-const DashboardWrapper = ({ children }: DashboardWrapperProps) => {
+const DashboardLayout = ({ children }: DashboardWrapperProps) => {
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed
+  );
+
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.add("light");
+    }
+  }, [isDarkMode]);
+
   return (
-    <div className={`light flex bg-gray-50 text-gray-900 w-full min-h-screen`}>
-      Sidebar
+    <div
+      className={`${
+        isDarkMode ? "dark" : "light"
+      } flex bg-gray-50 text-gray-900 w-full min-h-screen`}
+    >
+      <Sidebar />
       <main
-        className={`flex flex-col w-full h-full py-7 px-9 bg-gray-50 md:pl-24`}
+        className={`flex flex-col w-full h-full py-7 px-9 bg-gray-50 ${
+          isSidebarCollapsed ? "md:pl-24" : "md:pl-72"
+        }`}
       >
         <Navbar />
         {children}
       </main>
     </div>
+  );
+};
+
+const DashboardWrapper = ({ children }: DashboardWrapperProps) => {
+  return (
+    <StoreProvider>
+      <DashboardLayout>{children}</DashboardLayout>
+    </StoreProvider>
   );
 };
 
