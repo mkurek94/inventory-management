89187f216bef99eb3e8e994f3aabe2988e3d76cd
Mateusz Kurek <m.kurek1994@gmail.com>
2024-10-10 22:54:44 +0200

    add navbar

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -1,11 +1,22 @@
 import React from "react";
+import Navbar from '@/app/(components)/Navbar'
 
 type DashboardWrapperProps = {
   children: React.ReactNode;
 };
 
 const DashboardWrapper = ({ children }: DashboardWrapperProps) => {
-  return <div>DashboardWrapper{children}</div>;
+  return (
+    <div className={`light flex bg-gray-50 text-gray-900 w-full min-h-screen`}>
+      Sidebar
+      <main
+        className={`flex flex-col w-full h-full py-7 px-9 bg-gray-50 md:pl-24`}
+      >
+        <Navbar />
+        {children}
+      </main>
+    </div>
+  );
 };
 
 export default DashboardWrapper;
